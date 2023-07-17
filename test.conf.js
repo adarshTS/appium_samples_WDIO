b56809f9e02fa36f33acb6151e71394e863d999e
adarshTS <adarsh.ts8@gmail.com>
2023-07-17 13:14:05 +0530

    browserstack_config

---
+++ test.conf.js
@@ -0,0 +1,46 @@
+exports.config = {
+    user: process.env.BROWSERSTACK_USERNAME, 
+    key: process.env.BROWSERSTACK_ACCESS_KEY,    
+    hostname: 'hub.browserstack.com',
+    services: [
+      [
+        'browserstack',
+        {
+          app: 'bs://30c7440808d76bebf3f2d3b02b539a4015c87788',
+          buildIdentifier: "${BUILD_NUMBER}",
+          browserstackLocal: false
+        },
+      ]
+    ],
+    capabilities: [{
+      'bstack:options': {
+        deviceName: 'Samsung Galaxy S22 Ultra',
+        platformVersion: '12.0',
+        platformName: 'android',
+      }
+    }, {
+      'bstack:options': {
+        deviceName: 'Google Pixel 7 Pro',
+        platformVersion: '13.0',
+        platformName: 'android',
+      } }, {
+      'bstack:options': {
+        deviceName: 'OnePlus 9',
+        platformVersion: '11.0',
+        platformName: 'android',
+      }
+    }],
+    commonCapabilities: {
+      'bstack:options': {
+        projectName: "BrowserStack WDIO Sample",
+        buildName: "bstack-wdio-demo",
+        debug: true,
+        networkLogs: true
+      }
+    },
+    maxInstances: 10,
+    specs: [
+        'test/specs/**/*.js'
+       
+    ],
+}

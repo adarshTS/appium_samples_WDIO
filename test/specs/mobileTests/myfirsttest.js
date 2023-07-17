d6c55052fcd1edef7a5676f4ea6aed46f0bd73c1
adarshTS <adarsh.ts8@gmail.com>
2023-07-17 13:57:11 +0530

    initial visual regression

--- test/specs/mobileTests/myfirsttest.js
+++ test/specs/mobileTests/myfirsttest.js
@@ -1,8 +1,9 @@
+const percyScreenshot = require('@percy/appium-app');
 describe('e2e Test', ()=> {
-    it('Open Categories', () => {
+    it('Open Categories', async () => {
 
         const element = $('//android.widget.Button[@content-desc="tabButton_categories"]');
         element.click();
-
+        await percyScreenshot('Categories Page');
     })
 })
